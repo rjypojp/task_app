a723af9eccfb9b0611efbe2d3725064182356333
rjypojp <ryotarosu765@gmail.com>
2026-05-28 09:59:50 +0900

    restore secret key setup

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, redirect, url_for
+
 from routes.auth import auth_bp
 from routes.tasks import tasks_bp
 
+load_dotenv()
+
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(tasks_bp)
